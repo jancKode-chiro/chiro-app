738598a12001900e28007d7610a22b08698a802c
jancKode-chiro <jan@chiropracticadvertising.com>
2021-09-30 16:50:47 +0800

    Installed global state management -redux/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import CardHeader from '@material-ui/core/CardHeader';
+
 import './App.css';
+import RepsonsiveContainerGrid from './components/common/wrapper/grid-container';
+import ResponsiveCard from './components/common/wrapper/card';
+import { CardContent, TextField } from '@material-ui/core';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <RepsonsiveContainerGrid>
+        <Grid>
+          <ResponsiveCard>
+            <CardHeader>
+              <CardContent>
+                <TextField
+                  label='Enter your email'
+                  fullWidth
+                  autoFocus
+                  required
+                  inputRef={(input) => console.log('input')}
+                  error={false}
+                  helperText={''}
+                />
+              </CardContent>
+            </CardHeader>
+          </ResponsiveCard>
+        </Grid>
+      </RepsonsiveContainerGrid>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import store from './redux/store';
+
 import App from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
