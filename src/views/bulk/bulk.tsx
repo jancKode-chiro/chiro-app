7f75a3fae2486d5f26e6dfac87726c7e55a4c2ee
jancKode-chiro <jan@chiropracticadvertising.com>
2021-10-13 15:25:29 +0800

    upload page added

--- src/views/bulk/bulk.tsx
+++ src/views/bulk/bulk.tsx
@@ -3,30 +3,29 @@ import React from 'react';
 import ResponsiveContainerGrid from '../../components/common/wrapper/grid-container';
 import Button from '../../components/common/button/button';
 import verticalSpacer from '../../components/common/spacer/vertical-spacer';
-
+import WrapperWithImage from '../../components/common/wrapper/wrapper-with-image/wrapper-with-image';
 import { Input as AntInput } from 'antd';
 
 import './bulk.scss';
 
 const Bulk = () => {
   return (
-    <ResponsiveContainerGrid className='image'>
-      <div className='sms'>
-        <div>
-          <span className='bulk'>Bulk SMS</span>
-        </div>
-        {verticalSpacer('28px')}
-        <div>
+    <ResponsiveContainerGrid>
+      <WrapperWithImage>
+        <div className='sms'>
           <div>
-            <span className='search'>Search</span>
-            <div className='choosefile'>Choose File</div>
+            <span className='bulk'>Bulk SMS</span>
           </div>
-
           {verticalSpacer('28px')}
-
           <div>
-            <span className='bar'>MESSAGE</span>
+            <div className='group'>
+              <span className='search'>Search</span>
+              <button className='choosefile'>Choose File</button>
 
+              {verticalSpacer('28px')}
+
+              <span className='bar'>MESSAGE</span>
+            </div>
             <AntInput.TextArea rows={15} cols={50} />
 
             {verticalSpacer('16px')}
@@ -35,7 +34,7 @@ const Bulk = () => {
             </div>
           </div>
         </div>
-      </div>
+      </WrapperWithImage>
     </ResponsiveContainerGrid>
   );
 };
