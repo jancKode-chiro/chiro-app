c123298060998a56138094103f81d6de3349d18f
jancKode-chiro <jan@chiropracticadvertising.com>
2021-10-20 10:08:00 +0800

    added intergrating react hook forms

--- src/views/wallet/wallet.tsx
+++ src/views/wallet/wallet.tsx
@@ -1,39 +1,86 @@
-import React from 'react';
+import { withRouter } from 'react-router';
 
-import CardWithImage from '../../components/common/wrapper/card-with-image';
-import CustomInput from '../../components/common/forms/custom-input/custom-input';
-import Button from '../../components/common/button/button';
 import RepsonsiveContainerGrid from '../../components/common/wrapper/grid-container';
 import verticalSpacer from '../../components/common/spacer/vertical-spacer';
-import { Input } from '../../components/common/forms/custom-input/input';
+
+import { ContainerWithImage } from '../../components/common/wrapper/wrapper-with-image/wrapper-with-bg-image';
+import {
+  Input,
+  InputButton,
+} from '../../components/common/forms/custom-input/input';
 import { Input as AntInput } from 'antd';
 
 import './wallet.style.scss';
 
+import { useHistory } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useAuth } from '../../context/auth-context';
+
+type WalletProps = {
+  setAuth: () => Boolean;
+};
+
+type InputProps = {
+  cardDetails: string;
+  amountInUSD: string;
+};
+
 const Wallet = () => {
+  const { isAuth, setIsAuth } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  let history = useHistory();
+
+  const submitHandler: SubmitHandler<InputProps> = (data): void => {
+    history.push('/dashboard');
+    // setIsAuth(true);
+    console.log(data);
+  };
+
   return (
     <RepsonsiveContainerGrid className='image'>
-      <div>
-        <div className='box'>
-          <div>
-            <span className='available'>Available Balance</span>
-            <AntInput.TextArea rows={10} cols={50} />
-          </div>
-        </div>
-        {verticalSpacer('60px')}
-
-        <div className='text'>
-          <CustomInput name=' Card Details' placeholder='Card Details' />
-          {verticalSpacer('60px')}
-          <CustomInput name=' Topup amount' placeholder='Amount in USD' />
-        </div>
-        {verticalSpacer('90px')}
+      <ContainerWithImage>
         <div>
-          <Button className='bg-green text-white'>Save</Button>
+          <form className='container' onSubmit={handleSubmit(submitHandler)}>
+            <AntInput.TextArea
+              rows={12}
+              cols={41}
+              placeholder='Available Balance'
+              className='balance'
+              required
+              {...register('cardDetails', { required: true })}
+            />
+
+            {verticalSpacer('60px')}
+
+            <Input
+              placeholder='Card Details'
+              width='25vw'
+              required
+              {...register('cardDetails', { required: true })}
+            />
+            {verticalSpacer('40px')}
+            <Input
+              placeholder='Amount in USD'
+              width='25vw'
+              required
+              {...register('amountInUSD', { required: true })}
+            />
+
+            {verticalSpacer('50px')}
+
+            <InputButton
+              type='submit'
+              value='Save'
+              className='bg-green text-white'
+            />
+          </form>
         </div>
-      </div>
+      </ContainerWithImage>
     </RepsonsiveContainerGrid>
   );
 };
-
-export default Wallet;
+export default withRouter(Wallet);
