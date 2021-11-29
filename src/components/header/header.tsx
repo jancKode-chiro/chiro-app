5b849dfe97054adadb465fe72b86b50164035c00
jancKode-chiro <jan@chiropracticadvertising.com>
2021-11-29 20:58:59 +0800

    refactored the slide navigation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, FC } from 'react';
 
-import SideBarNavigation from '../../components/sidebar-navigation/sidebar-data/sidebar-data';
+import SideBar from '../../components/sidebar-navigation/sidebar-data/sidebar-data';
 import SideBarProfileData from '../../components/sidebar-navigation/sidebar-profile-data/sidebar-profile-data';
 
 import './header.styles.scss';
@@ -14,7 +14,7 @@ const CustomHeader: FC<HeaderProps> = ({ title }) => {
 
   return (
     <header className='header'>
-      <SideBarNavigation />
+      <SideBar />
       <h1 className='page-title'>{title} </h1>
       <SideBarProfileData />
     </header>
