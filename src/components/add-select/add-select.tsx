a6790227efef56f1680961b1211058000c1c65df
jancKode-chiro <jan@chiropracticadvertising.com>
2021-12-13 19:45:55 +0800

    added add user function

---
+++ src/components/add-select/add-select.tsx
@@ -0,0 +1,44 @@
+
+import Select, { StylesConfig } from 'react-select';
+import makeAnimated from 'react-select/animated'
+
+
+const animatedComponents = makeAnimated();
+
+const countryOptions = [
+  { value: 'Admin', label: 'Admin' },
+  { value: 'User', label: 'User' },
+
+]
+
+const customStyles: StylesConfig = {
+  input: (styles) => ({
+    ...styles,
+    height: '40px',
+    color: 'transparent',
+  }),
+  multiValue: (styles) => {
+    return {
+      ...styles,
+      backgroundColor: 'transparent'
+    }
+  },
+  multiValueLabel: styles => {
+    return {
+      color: 'rgb(61 151 122 / 70%)'
+    }
+  }
+}
+
+const AddSelect = () => {
+  return (
+    <Select
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      options={countryOptions}
+      styles={customStyles}
+    />
+  )
+}
+
+export default AddSelect
