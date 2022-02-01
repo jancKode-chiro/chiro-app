63786df44ca66218b55b29da482e3117b02daee2
jancKode-chiro <jan@chiropracticadvertising.com>
2022-02-01 08:53:43 +0800

    dashboard C data

--- src/views/dashboard/navigation/side-drawer/sidedrawer.tsx
+++ src/views/dashboard/navigation/side-drawer/sidedrawer.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useCallback, useRef } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
 
 import {
   Drawer,
@@ -9,110 +7,23 @@ import {
   Divider,
   Typography,
   Box,
-  List,
-  Tooltip,
-  withStyles,
-  ListItemIcon,
-  ListItem
+  withStyles
 } from "@material-ui/core";
-
-import { RiCloseCircleLine } from "react-icons/ri";
-
-import HelpOutline from "@material-ui/icons/HelpOutline";
-import Payment from "@material-ui/icons/Payment"
-import Card from "@material-ui/icons/CardMembershipOutlined"
+import CloseIcon from "@material-ui/icons/Close";
 
 const drawerWidth = 240;
 
-
-const styles = (theme: any) => ({
+const styles = {
   toolbar: {
     minWidth: drawerWidth
-  },
-  textPrimary: {
-    color: theme.palette.primary.main,
-  },
-  mobileItemSelected: {
-    backgroundColor: `${theme.palette.primary.main} !important`,
-  },
-})
+  }
+};
+
 
 
 function SideDrawer(props: any) {
-  const {
-    selectedTab,
-    messages,
-    classes,
-    width,
-    openAddBalanceDialog,
-    onClose,
-    open
-  }
-    = props;
 
-  const links = useRef<any>([]);
-  const [isMobileOpen, setIsMobileOpen] = useState(false);
-
-  const closeMobileDrawer = useCallback(() => {
-    setIsMobileOpen(false);
-  }, [setIsMobileOpen]);
-
-
-
-  const sideDrawerItems = [
-    {
-      link: "profile-info",
-      name: "Profile Information ",
-      onClick: closeMobileDrawer,
-      icon: {
-        desktop: (
-          <HelpOutline
-            className={
-              selectedTab === "Profile Information " ? classes.textPrimary : "text-black"
-            }
-            fontSize="large"
-          />
-        ),
-        mobile: <HelpOutline className="text-black" />,
-      },
-    },
-    {
-      link: "wallet",
-      name: "Payment Method",
-      onClick: closeMobileDrawer,
-      icon: {
-        desktop: (
-          <Payment
-            className={
-              selectedTab === "PaymentMethod" ? classes.textPrimary : "text-black"
-            }
-            fontSize="large"
-          />
-        ),
-        mobile: <Payment className="text-black" />,
-      },
-    },
-    {
-      link: "card",
-      name: "Card",
-      onClick: closeMobileDrawer,
-      icon: {
-        desktop: (
-          <Card
-            className={
-              selectedTab === "Card" ? classes.textPrimary : "text-black"
-            }
-            fontSize="large"
-          />
-        ),
-        mobile: <Card className="text-black" />,
-      },
-    },
-  ]
-
-
-
-
+  const { classes, onClose, open } = props;
   return (
     <Drawer anchor="right" open={open} variant="temporary" onClose={onClose}>
       <Toolbar disableGutters className={classes.toolbar}>
@@ -124,51 +35,17 @@ function SideDrawer(props: any) {
           width="100%"
           alignItems="center"
         >
-          {/* <Typography variant="h6">Information Section</Typography> */}
+          <Typography variant="h6">Information Section</Typography>
           <IconButton
             onClick={onClose}
             color="primary"
             aria-label="Close Sidedrawer"
           >
-            <RiCloseCircleLine fontSize={35} />
+            <CloseIcon />
           </IconButton>
-
         </Box>
       </Toolbar>
       <Divider />
-      <List>
-        {sideDrawerItems.map((element, index) => (
-          <Link
-            to={element.link}
-            className={classes.menuLink}
-            onClick={element.onClick}
-            key={index}
-            ref={(node: any) => {
-              links.current[index] = node;
-            }}
-          >
-            <Tooltip
-              title={element.name}
-              placement="right"
-              key={element.name}
-            >
-              <ListItem
-                selected={selectedTab === element.name}
-                button
-                divider={index !== sideDrawerItems.length - 1}
-                className={classes.permanentDrawerListItem}
-                onClick={() => {
-                  links.current[index].click();
-                }}
-              >
-                <ListItemIcon className={classes.justifyCenter}>
-                  {element.icon.desktop}
-                </ListItemIcon>
-              </ListItem>
-            </Tooltip>
-          </Link>
-        ))}
-      </List>
     </Drawer>
   );
 }
