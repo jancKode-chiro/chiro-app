c5471f36b36d2e01754cf058c1f09eb4990ac8b6
jancKode-chiro <jan@chiropracticadvertising.com>
2022-03-22 14:34:46 +0800

    sms-scheduler-message

--- src/views/sms-page/sms-date-picker/sms-date-picker.tsx
+++ src/views/sms-page/sms-date-picker/sms-date-picker.tsx
@@ -1,25 +1,42 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, SetStateAction } from 'react'
 import { toast } from 'react-toastify'
 import { useForm, Controller, SubmitHandler } from 'react-hook-form'
-import { KeyboardTimePicker } from '@material-ui/pickers'
+import { DateTimePicker } from '@material-ui/pickers'
 import DatePicker from 'react-datepicker'
+import React from 'react'
 
 
 const SmsDatePicker = () => {
+  const [date, setDate] = useState();
+
   const loadDate = useRef(new Date() as any)
 
   const { control } = useForm()
 
-  const [myDate, setMyDate] = useState<Date | null>(null)
-  const [selectedTime, handleTimeChange] = useState(new Date());
+  // const [myDate, setMyDate] = useState<Date | null>(null)
+
+  const [clearedDate, handleClearedDateChange] = useState(null);
+  const [selectedTime, handleTimeChange] = React.useState(new Date());
   const [selectDate, setSelectDate] = useState<Date | null>(null)
   const notifyDate = () => toast.update(loadDate.current, { render: 'Schedule has been set', type: toast.TYPE.SUCCESS, autoClose: 10000 })
-  const onSelectDateHandler = async (data: Date): Promise<void> => {
+
+  const onSelectDateHandler = async (date: any): Promise<void> => {
     setSelectDate(selectDate)
     notifyDate();
     // clearDayTimeout()   
-    console.log(data)
+    console.log(date)
   }
+
+  // const handleSelectDate = (date: any) => {
+
+  // }
+
+  const handleDateChange = (date: any) => {
+    setDate(date);
+  };
+
+  // console.log(date)
+
   return (
     <>
       <Controller
@@ -28,23 +45,35 @@ const SmsDatePicker = () => {
         defaultValue={null}
         render={({ field }) => (
           <>
-            <DatePicker
+            <DateTimePicker
+              label="DateTimePicker"
+              inputVariant="outlined"
+              value={selectedTime}
+              color='secondary'
+              fullWidth
+              onChange={onSelectDateHandler}
+            />
+            {/* <DatePicker
               onChange={(e) => field.onChange((e), onSelectDateHandler)}
               selected={field.value}
               placeholderText="Select date"
+              showTimeInput
               dateFormat="Pp"
               isClearable
               withPortal
-            />
-            <br />
-            <KeyboardTimePicker
+            /> */}
+            {/* <br /> */}
+            {/* <KeyboardTimePicker
               autoOk
-              variant='static'
+              // variant='static'
               openTo='hours'
               placeholder="Select Time"
               mask="__:__ _M"
               value={selectedTime}
-              onChange={(date: any) => handleTimeChange(date)} /></>
+              onChange={(date: any) => handleDateChange(date)}
+
+            /> */}
+          </>
         )}
 
       />
